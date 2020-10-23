0d9d298d2bdcd25929b4b82a50604c5562870276
abhishek-aasarawa-au7 <aasarawa@gmail.com>
2020-10-23 15:29:20 +0530

    search paths result improved and sorted

--- server/utils/findPaths.js
+++ server/utils/findPaths.js
@@ -1,14 +1,15 @@
-import { isEqual } from "lodash";
+import { isEqual, isEmpty } from "lodash";
 
 import userModel from "../models/users.model";
 
-const allPathUtils = async (u, d, visited, path, all) => {
+const allPathUtils = async (u, d, visited, path, all, reverse = false) => {
   visited.push(u.name);
 
   path.push(u.name);
 
   if (u.name === d.name) {
     let paths = [...path];
+    if (reverse) paths = paths.reverse();
     if (!isEqual(all[all.length - 1], paths)) all.push(paths);
   } else {
     u = await userModel
@@ -18,7 +19,7 @@ const allPathUtils = async (u, d, visited, path, all) => {
 
     for (let i of u.relationWith) {
       if (!visited.includes(i.name))
-        await allPathUtils(i, d, visited, path, all);
+        await allPathUtils(i, d, visited, path, all, reverse);
     }
   }
 
@@ -32,6 +33,13 @@ const allPath = async (s, d) => {
   let paths = [];
 
   await allPathUtils(s, d, visited, paths, all);
+
+  // if there is no path
+  if (isEmpty(all)) {
+    await allPathUtils(d, s, [], [], all, true);
+  }
+
+  all.sort((a, b) => (a.length < b.length ? -1 : 1));
 
   return all;
 };
